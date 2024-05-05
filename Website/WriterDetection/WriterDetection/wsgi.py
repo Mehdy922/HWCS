eff08a03ad74a2d5c173ea1e6f0d16b96a57409c
Muhammad Hasnain Fatmi <hasnainfatmi22@gmail.com>
2024-05-05 17:02:41 +0500

    update

--- Website/WriterDetection/WriterDetection/wsgi.py
+++ Website/WriterDetection/WriterDetection/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for PneumoniaDetectionSystem project.
+WSGI config for WriterDetection project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'PneumoniaDetectionSystem.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WriterDetection.settings')
 
 application = get_wsgi_application()
